b80d124b897165ad36aa31f31d9e99c755c1337f
Daniel Wedin <danielwedin@hotmail.se>
2018-04-24 22:25:35 +0200

    Restructure stories and add speed example

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,52 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
 
-import "./stories.css";
+import { configure } from "@storybook/react";
 
-import Marquee from "../src/marquee";
-import ExampleWrapper from "./exampleWrapper";
-import ExampleParagraph from "./exampleParagraph";
+const req = require.context("./examples", true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
 
-storiesOf("Marquee", module).add("with text", () => (
-  <Marquee>
-    <p style={{ background: "LightGoldenRodYellow", display: "block", width: "100%", padding: "15px" }}>Yolo swag stuff barbecue</p>
-  </Marquee>
-));
-
-storiesOf("Marquee", module).add(
-  "with direction",
-  withInfo(`
-  Sets the direction of the scrolling within the marquee.
-  Possible values are LEFT, RIGHT, UP and DOWN.
-
-  If no value is specified, the default value is LEFT.
-`)(() => (
-  <React.Fragment>
-    <ExampleWrapper>
-      <Marquee direction="LEFT">
-        <ExampleParagraph text="This text will scroll from left to right" />
-      </Marquee>
-    </ExampleWrapper>
-
-    <ExampleWrapper>
-      <Marquee direction="RIGHT">
-        <ExampleParagraph background="AliceBlue" text="This text will scroll from right to left" />
-      </Marquee>
-    </ExampleWrapper>
-
-    <ExampleWrapper>
-      <Marquee direction="UP">
-        <ExampleParagraph background="PaleVioletRed" text="This text will scroll from bottom to top" />
-      </Marquee>
-    </ExampleWrapper>
-
-    <ExampleWrapper>
-      <Marquee direction="DOWN">
-        <ExampleParagraph background="PeachPuff" text="This text will scroll from top to bottom" />
-      </Marquee>
-    </ExampleWrapper>
-  </React.Fragment>
-  ))
-);
+configure(loadStories, module);
